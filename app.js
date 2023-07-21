1c6a1262569077b198bf007204ac80b94585d851
Spyravis <ritalopezcobo@gmail.com>
2023-07-21 22:57:16 +0000

    done

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const sum = (a,b) => { 
-    return a + b 
+        return a + b
 }
 const fromEuroToDollar = function(valueInEuro){
     let valueInDollar = valueInEuro * 1.2;    
     return valueInDollar; 
 }
-const fromDollarToYen = function(valueInDollar){ 
+const fromDollarToYen = function(valueInDollar){
     let valueInYen = valueInDollar * 128.7;   
-    return valueInYen; 
+    return valueInYen;
 }
-const fromYenToPound = function(valueInYen){ 
-    let valueInPound = valueInYen * 0.0062;   
+const fromYenToPound = function(valueInYen){
+    let valueInPound = valueInYen * 0.0062; 
     return valueInPound; 
 }
 module.exports = { sum, fromYenToPound, fromDollarToYen, fromEuroToDollar }; 
